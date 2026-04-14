ebbb84fb06198964332b48b2f1122aa125ccc96c
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-04-14 16:26:04 +0200

    Conexion

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 
 mongoose.connect('mongodb://jesusobispo2005_db_user:IYBe5rsErWcSuNMG@ac-yhhifoq-shard-00-00.z3vse1u.mongodb.net:27017,ac-yhhifoq-shard-00-01.z3vse1u.mongodb.net:27017,ac-yhhifoq-shard-00-02.z3vse1u.mongodb.net:27017/?ssl=true&replicaSet=atlas-ai6ulp-shard-0&authSource=admin&appName=Cluster0')
-.then(() => console.log('✅ Conectado'))
-.catch(err => console.log('❌ Error:', err));
+.then(() => {
+    console.log('Conexión exitosa a MongoDB');
+})
+.catch((error) => {
+    console.error('Error al conectar a MongoDB:', error);
+});
